75c4703180002cb36f4b89abab2a1f443284c496
vivek012 <vivekk186388@gmail.com>
2025-11-10 13:44:01 +0530

    fixing Error

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -53,7 +53,6 @@ const Sidebar = () => {
                   onlineUsers.includes(user._id)
                     ? <span className='text-green-400 text-xs'>Online</span>
                     : <span className=" text-neutral-400 text-xs">Offline</span>
-
                 }
               </div>
               {unseenMessages[user._id] > 0 && <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center rounded-full bg-violet-500/50'>{unseenMessages[user._id]}</p>}
